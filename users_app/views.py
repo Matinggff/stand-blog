d0b0a5a6e1491d4cbe1f8d59de97007060cc2757
matin salehi <symatin3@gmail.com>
2026-06-14 21:39:47 +0330

    add login and logout view

--- users_app/views.py
+++ users_app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-
-def login(request):
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('pass')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                pass
     return render(request, "users_app/login.html", {})
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
