84e3e560a446702de04276991409ea7f95c90151
matin salehi <symatin3@gmail.com>
2026-07-01 18:53:11 +0330

    feat(blog): implemented a simple search for posts

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('detail/<slug:slug>', views.post_detail, name='detail'),
     path('list', views.post_list, name='post_list'),
     path('category/<slug:slug>', views.category_detail, name='category_detail'),
+    path('search', views.search, name='search'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -34,3 +34,11 @@ def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     posts = category.posts.all()
     return render(request, 'blog_app/blog.html', {'posts': posts})
+
+def search(request):
+    query = request.GET.get('q')
+    posts = Post.objects.filter(title__icontains=query)
+    paginator = Paginator(posts, 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog_app/blog.html', {'posts': page_obj})
